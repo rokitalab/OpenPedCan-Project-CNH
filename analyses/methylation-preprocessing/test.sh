f6ccf71e415d8ac364859c76298a169834a5f7d8
chaodi51 <dichao83@gmail.com>
2025-08-05 15:40:15 -0400

    sync methylation-preprocessing/ from dev

--- analyses/methylation-preprocessing/test.sh
+++
@@ -1,3 +0,0 @@
-cwltool  --user-space-docker-cmd="docker" --outdir outputs/current_Rscript workflow/methylation-preprocessing.cwl workflow/methylation-preprocessing-input.yml
-
- docker run -it --rm -v $PWD:$PWD dmiller15/minfi:4.2.0 bash
