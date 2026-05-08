17b038b681a41899955d064e631058009f101773
jessied2139 <jessieucas21@gmail.com>
2026-05-08 12:47:39 -0400

    continue working on cnv calls

--- analyses/methylation-preprocessing/scripts/03-cnv-calls.R
+++ analyses/methylation-preprocessing/scripts/03-cnv-calls.R
@@ -2,6 +2,13 @@
 # Jessica Daggett
 # 05/07/2026
 
+Sys.setenv(
+  OMP_NUM_THREADS = 1,
+  OPENBLAS_NUM_THREADS = 1,
+  MKL_NUM_THREADS = 1,
+  VECLIB_MAXIMUM_THREADS = 1,
+  NUMEXPR_NUM_THREADS = 1
+)
 
 # Load libraries:
 suppressPackageStartupMessages(library(optparse))
@@ -45,11 +52,42 @@ out_base <- opt$output_basename
 
 base_dir <- 'sorted_idats_output_dir/IlluminaHumanMethylationEPIC'
 n_cores <- 4
-manifest_file <- 'input-test/controls_and_dicer_manifest.tsv'
+manifest_file <- 'controls_and_dicer_manifest.tsv'
 out_base <- 'test-out/'
+dataset <- basename(base_dir)
+
 
 m_set_file <- paste0(out_base, "-", dataset, '-m-set.qs2')
 GRset <- qs_read(m_set_file)
 
 
 anno <- CNV.create_anno(array_type = "EPIC") 
+
+# read manifest to obtain the IDAT prefix from the `file_name` and its matched `Bioassay_ID` column
+man_df <- read_tsv(file = manifest_file, show_col_types = FALSE) %>% 
+  dplyr::select(file_name, Bioassay_ID, sample_type, platform) %>%
+  dplyr::mutate(file_name = gsub("(_Red|_Grn).*", "", file_name)) %>%
+  dplyr::mutate(file_name = basename(file_name)) %>%
+  dplyr::filter(platform == "HumanMethylationEPIC") %>%
+  unique()
+
+normals <- man_df %>% 
+  dplyr::filter(sample_type == 'Normal') %>%
+  dplyr::select(file_name)
+
+reference_samples <- intersect(normals$file_name, colnames(GRset))
+
+
+
+query <- CNV.load(
+  GRset[, '204305120005_R08C01'],
+  anno = anno
+)
+
+
+
+cnv <- conumee2::CNV.fit(
+  GRset[, '204305120005_R08C01'],
+  GRset[, reference_samples],
+  anno
+)
